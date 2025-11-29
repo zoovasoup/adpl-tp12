2d764074ac6c4b05be6a8cfaa15f349b1743dfd5
zoovasoup <zulfa.plank@proton.me>
2025-11-30 00:39:17 +0700

    [feat] product route from user with bean

--- user-service/src/main/java/com/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/user_service/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +14,9 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate() { 
+		return new RestTemplate();
+	}
 }
--- user-service/src/main/java/com/user_service/controller/UserController.java
+++ user-service/src/main/java/com/user_service/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.user_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@RestController 
-@RequestMapping("/User") 
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/")
-	public String getUser() { 
+	public String getUser() {
 		return "Test User";
+	}
+
+	@GetMapping("/product")
+	public String getProductMessage() {
+		String productMessage = restTemplate.getForObject("http://localhost:8082/product/", String.class);
+		return "User Service calling Product Service: " + productMessage;
 	}
 }
